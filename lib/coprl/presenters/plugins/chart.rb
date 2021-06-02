670613e079b4658fd48ea795b1700a3578ecbbed
Rx <russell@russelledens.net>
2021-06-02 12:30:44 -0400

    fix: Migrated from presenters v2 to COPRL presenters v3
    BREAKING CHANGE: This version of the chart is only compatible with COPRL presenters v3. It does not work with presenters v2.

--- lib/coprl/presenters/plugins/chart.rb
+++ lib/coprl/presenters/plugins/chart.rb
@@ -1,6 +1,6 @@
 require_relative './chart/component'
 
-module Voom
+module Coprl
   module Presenters
     module Plugins
       module Chart
@@ -10,20 +10,21 @@ module Voom
           end
         end
         module WebClientComponents
+          def view_dir_chart(pom)
+            File.join(__dir__, '../../../../views/chart')
+          end
           # The string returned from this method will be added to the HTML header section of the page layout
           # It will be called once for the page.
           # The pom is passed along with the sinatra render method.
-          def render_header_chart(_pom, render:)
-            view_dir = File.join(__dir__, 'chart')
-            render.call :erb, :header, views: view_dir
+          def render_header_chart(pom, render:)
+            render.call :erb, :header, views: view_dir_chart(pom)
           end
 
           def render_chart(comp,
                            render:,
                            components:,
                            index:)
-            view_dir = File.join(__dir__, 'chart')
-            render.call :erb, :chart, views: view_dir,
+            render.call :erb, :chart, views: view_dir_chart(components),
                         locals: {comp: comp,
                                  components: components, index: index}
           end
