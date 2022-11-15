02a39ab404cb7aa94728d47462597a2ce9240a73
Russell Edens <russell@voomify.com>
2022-11-15 15:59:22 -0500

    WIP node-sass update

--- views/chart/build.sh
+++ views/chart/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-npm i
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm install `cat .nvmrc`
+nvm use `cat .nvmrc`
+npm install
 cp node_modules/d3/dist/d3.js ../../public
 cp node_modules/c3/c3.js ../../public
 cp node_modules/c3/c3.css ../../public
